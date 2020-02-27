3ae9e8607d77b712539c34ba4ec454282bd02161
Francisco Manzano <keoh77@gmail.com>
2020-02-27 03:29:38 +0100

    queue script fix

--- queue.py
+++ queue.py
@@ -7,50 +7,39 @@ NUM_TASKS = 20
 
 async def worker(name, queue):
     while True:
-        # Get a "work item" out of the queue.
         sleep_for = await queue.get()
-
-        # Sleep for the "sleep_for" seconds.
         await asyncio.sleep(sleep_for)
-
-        # Notify the queue that the "work item" has been processed.
         queue.task_done()
-
         print(f'{name} has slept for {sleep_for:.2f} seconds')
 
 
-
 async def main():
-    # Create a queue that we will use to store our "workload".
     queue = asyncio.Queue()
 
-    # Generate random timings and put them into the queue.
     total_sleep_time = 0
     for _ in range(NUM_TASKS):
         sleep_for = random.uniform(0.05, 1.0)
         total_sleep_time += sleep_for
         queue.put_nowait(sleep_for)
 
-    # Create three worker tasks to process the queue concurrently.
     tasks = []
     for i in range(WORKERS):
         task = asyncio.create_task(worker(f'worker-{i}', queue))
         tasks.append(task)
 
-    # Wait until the queue is fully processed.
     started_at = time.monotonic()
     await queue.join()
     total_slept_for = time.monotonic() - started_at
 
-    # Cancel our worker tasks.
     for task in tasks:
         task.cancel()
-    # Wait until all worker tasks are cancelled.
-    cosas = await asyncio.gather(*tasks, return_exceptions=True)
-    print(cosas)
-    print('====')
+
+    await asyncio.gather(*tasks, return_exceptions=True)
+
+    print('='*10)
     print(f'{WORKERS} workers slept in parallel for {total_slept_for:.2f} seconds')
     print(f'total expected sleep time: {total_sleep_time:.2f} seconds')
+    print('='*10)
 
 
 asyncio.run(main())
